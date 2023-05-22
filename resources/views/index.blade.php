ea2f290efcf58a76ecbaf1ba62a4d291cf1f6b11
jessiline <95264872+jessiline@users.noreply.github.com>
2023-05-22 13:05:53 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,18 +36,7 @@
     </div>
 @endif
 
-    <!-- ? Preloader Start -->
-    {{-- <div id="preloader-active">
-      <div class="preloader d-flex align-items-center justify-content-center">
-        <div class="preloader-inner position-relative">
-          <div class="preloader-circle"></div>
-          <div class="preloader-img pere-text">
-            <img src="{{ asset('assets/images/logo/logo.png') }}" alt="" />
-          </div>
-        </div>
-      </div>
-    </div --}}
-    <!-- Preloader Start-->
+
     @include('header')
     <!-- header end -->
     <main>
