e88341239ddf86ddfecddf4369724ebe1394c4f4
jessiline <95264872+jessiline@users.noreply.github.com>
2023-06-09 00:31:22 +0700

    Update product_details.blade.php

--- resources/views/product_details.blade.php
+++ resources/views/product_details.blade.php
@@ -86,28 +86,6 @@ if (isset($_POST['product_name'])) {
   {{ session('success') }}
 </div>
 @endif
-  @include('header')
-  @if ($errors->any())
-  <div class="alert alert-danger alert-dismissible">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-      </button>
-      <ul>
-          @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-          @endforeach
-      </ul>
-  </div>
-  @endif
-
-  @if (session('success'))
-  <div class="alert alert-success alert-dismissible">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-      </button>
-      {{ session('success') }}
-  </div>
-  @endif
     <main>
         <!-- breadcrumb Start-->
         <div class="page-notification">
